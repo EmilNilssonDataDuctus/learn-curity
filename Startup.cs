9ed5dbe8302528e411356daef8d7148b0e37760d
Emil Nilsson <emil.nilsson@dataductus.se>
2023-01-30 15:15:33 +0100

    Change secret

--- Startup.cs
+++ Startup.cs
@@ -30,6 +30,8 @@ namespace OidcClientDemoApplication
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            Console.WriteLine(Configuration.GetValue<string>("OpenIdConnect:ClientSecret"));
+
             app.UseRouting();
             app.UseAuthentication();
             app.UseAuthorization();
